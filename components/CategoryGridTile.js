73fcc83bbd4599085b195b085134450ab2c28cd4
Erdem Özbaykuş <erdemozbaykus@gmail.com>
2022-11-14 12:37:57 +0300

    Base

---
+++ components/CategoryGridTile.js
@@ -0,0 +1,17 @@
+/** @format */
+
+import { Pressable, Text, View } from "react-native";
+
+function CategoryGridTile({ title, color }) {
+  return (
+    <View>
+      <Pressable>
+        <View>
+          <Text>{title}</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+}
+
+export default CategoryGridTile;
